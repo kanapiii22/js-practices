644b30d694909ce5c7a04e074004ee2fa70ada1f
kanapiii22 <205907063+kanapiii22@users.noreply.github.com>
2025-06-16 15:44:39 +0900

    年と月のバリデーション

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -5,3 +5,11 @@ const today = new Date();
 const year = args.y || today.getFullYear();
 const month = args.m || today.getMonth();
 console.log(args,today,year,month)
+
+
+if (year < 1970 || year > 2100) {
+  console.log('対応している年は1970年から2100年までです')
+}
+if (month < 1 || month > 12) {
+  console.log('月は1〜12の範囲で指定してください')
+}
